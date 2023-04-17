88ab44322b404700b928b7f8d9509234538817e1
Cristian Sirbu <cmsirbu@users.noreply.github.com>
2023-04-17 17:35:14 +0100

    Punctuation

--- pylint_nautobot/utils.py
+++ pylint_nautobot/utils.py
@@ -1,10 +1,10 @@
-"""Utilities for managing data"""
+"""Utilities for managing data."""
 from yaml import safe_load
 from importlib.resources import files
 
 
 def load_v2_code_location_changes():
-    """Black magic data transform, needs schema badly"""
+    """Black magic data transform, needs schema badly."""
     changes = safe_load(open(files("pylint_nautobot") / "data" / "v2" / "v2-code-location-changes.yaml"))
     changes_map = {}
     for change in changes:
