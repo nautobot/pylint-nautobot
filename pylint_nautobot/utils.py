7b20d5b30a5622d5aced1043c934521519d9fe5b
Cristian Sirbu <cmsirbu@users.noreply.github.com>
2023-04-18 16:44:13 +0100

    Fix importlib and pylint issues

--- pylint_nautobot/utils.py
+++ pylint_nautobot/utils.py
@@ -1,11 +1,12 @@
 """Utilities for managing data."""
+from importlib_resources import files
 from yaml import safe_load
-from importlib.resources import files
 
 
 def load_v2_code_location_changes():
     """Black magic data transform, needs schema badly."""
-    changes = safe_load(open(files("pylint_nautobot") / "data" / "v2" / "v2-code-location-changes.yaml"))
+    with open(files("pylint_nautobot") / "data" / "v2" / "v2-code-location-changes.yaml", encoding="utf-8") as rules:
+        changes = safe_load(rules)
     changes_map = {}
     for change in changes:
         if change["Old Module"] not in changes_map:
