b2bd968812616483d00afd2dc99358c7892e8b1e
Hanlin Miao <hanlin980505@gmail.com>
2023-06-22 12:11:00 -0400

    address PR feedback

--- pylint_nautobot/status_field_instead_of_status_model.py
+++ pylint_nautobot/status_field_instead_of_status_model.py
@@ -1,10 +1,10 @@
-"""Check for deprecated StatusModel usage and encourage the usage of StatusField instead."""
+"""Check for deprecated `StatusModel` usage and encourage the usage of `StatusField` instead."""
 from pylint.checkers import BaseChecker
 from pylint.interfaces import IAstroidChecker
 
 
 class NautobotDeprecatedStatusModelChecker(BaseChecker):
-    """Discourage the usage of deprecated StatusModel and encourage the usage of StatusField."""
+    """Discourage the usage of deprecated `StatusModel` and encourage the usage of `StatusField`."""
 
     __implements__ = IAstroidChecker
 
@@ -13,13 +13,14 @@ class NautobotDeprecatedStatusModelChecker(BaseChecker):
     name = "nautobot-status-field-instead-of-status-model"
     msgs = {
         "E4292": (
-            "Declare status on the model explicitly with a StatusField, as StatusModel is deprecated",
+            "Declare status on the model explicitly with a `StatusField`, as `StatusModel` is deprecated",
             "nb-status-field-instead-of-status-model",
             "https://docs.nautobot.com/projects/core/en/next/models/extras/status/#status-internals",
         ),
     }
 
     def visit_classdef(self, node):
-        for basename in node.basenames:
-            if basename in ["StatusModel", "statuses.StatusModel"]:
-                self.add_message("nb-status-field-instead-of-status-model", node=node)
+        # Wait until pylint-nautobot #9 is merged.
+        ancestor_class_types = [ancestor.qname() for ancestor in node.ancestors()]
+        if "nautobot.extras.models.statuses.StatusModel" in ancestor_class_types:
+            self.add_message("nb-status-field-instead-of-status-model", node=node)
