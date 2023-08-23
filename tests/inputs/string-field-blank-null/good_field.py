fb29901726dc134135c8dace7a588ad743783c61
Cristian Sirbu <cmsirbu@users.noreply.github.com>
2023-08-23 21:36:34 +0100

    Run black

--- tests/inputs/string-field-blank-null/good_field.py
+++ tests/inputs/string-field-blank-null/good_field.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.db.models import CharField
 from nautobot.core.models import BaseModel
 
+
 class MyModelOne(BaseModel):
     name = CharField(blank=True)
 
 
 class MyModelTwo(BaseModel):
     name = CharField(null=True)
+
 
 class MyModelThree(BaseModel):
     name = models.TextField(null=True)
