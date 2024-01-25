abb39deea1291c00ad6782e1b1f10aefbeea9263
Jan Snasel <jan.snasel@networktocode.com>
2024-01-25 13:33:05 +0000

    fix: Read nautobot version from poetry.lock or environment

--- pylint_nautobot/__init__.py
+++ pylint_nautobot/__init__.py
@@ -1,18 +1,16 @@
 """Initialization file for library."""
 from importlib import metadata
-from pathlib import Path
 
-import toml
 from packaging.specifiers import SpecifierSet
-from packaging.version import Version
 from pylint.lint import PyLinter
 
-from pylint_nautobot.code_location_changes import NautobotCodeLocationChangesChecker
-from pylint_nautobot.deprecated_status_model import NautobotDeprecatedStatusModelChecker
-from pylint_nautobot.incorrect_base_class import NautobotIncorrectBaseClassChecker
-from pylint_nautobot.model_label import NautobotModelLabelChecker
-from pylint_nautobot.replaced_models import NautobotReplacedModelsImportChecker
-from pylint_nautobot.string_field_blank_null import NautobotStringFieldBlankNull
+from .code_location_changes import NautobotCodeLocationChangesChecker
+from .deprecated_status_model import NautobotDeprecatedStatusModelChecker
+from .incorrect_base_class import NautobotIncorrectBaseClassChecker
+from .model_label import NautobotModelLabelChecker
+from .replaced_models import NautobotReplacedModelsImportChecker
+from .string_field_blank_null import NautobotStringFieldBlankNull
+from .utils import MINIMUM_NAUTOBOT_VERSION
 
 __version__ = metadata.version(__name__)
 
@@ -25,38 +23,9 @@ CHECKERS = [
     NautobotStringFieldBlankNull,
 ]
 
-
-def _read_target_pyproject_toml() -> dict:
-    """Try to discover the target projects 'pyproject.toml' to access its pylint-nautobot configuration."""
-    # TODO: It would be great if we could infer this from the Nautobot dependency constraint for the target project.
-    for directory in [*Path.cwd().parents, Path.cwd()]:
-        pyproject_toml_path = directory / "pyproject.toml"
-        if pyproject_toml_path.exists():
-            return toml.load(Path(pyproject_toml_path))
-
-    raise RuntimeError("Unable to find pyproject.toml for target project.")
-
-
 def register(linter: PyLinter):
     """Pylint plugin entrypoint - register all the checks to the linter."""
-    pyproject_toml_content = _read_target_pyproject_toml()
-
-    try:
-        # TBD: Read minimal supported Nautobot dependency version from poetry.
-        # Can be `dev` dependency as well.
-        # If there is no Nautobot dependency, use installed Nautobot version.
-        # Use single version only, not a list.
-        supported_nautobot_versions = [
-            Version(version)
-            # pylint: disable
-            for version in pyproject_toml_content["tool"]["pylint-nautobot"]["supported_nautobot_versions"]
-        ]
-    except KeyError as error:
-        raise ValueError("[tool.pylint-nautobot] configuration missing from pyproject.toml.") from error
-
     for checker in CHECKERS:
-        version_specifier_set = SpecifierSet(checker.version_specifier)
-        if not version_specifier_set or any(
-            (version in version_specifier_set for version in supported_nautobot_versions)
-        ):
+        checker_versions = SpecifierSet(checker.version_specifier)
+        if not checker_versions or MINIMUM_NAUTOBOT_VERSION in checker_versions:
             linter.register_checker(checker(linter))
--- pylint_nautobot/utils.py
+++ pylint_nautobot/utils.py
@@ -1,20 +1,17 @@
 """Utilities for managing data."""
+from importlib import metadata
+from pathlib import Path
+from typing import Optional
+
+import toml
 from importlib_resources import files
+from packaging.version import Version
 from yaml import safe_load
 
 
 def is_nautobot_v2_installed() -> bool:
     """Return True if Nautobot v2.x is installed."""
-    try:
-        # pylint: disable-next=import-outside-toplevel
-        from importlib.metadata import version
-
-        # pylint: disable-next=import-outside-toplevel
-        from packaging.version import Version
-
-        return Version(version("nautobot")).major == 2
-    except ImportError:
-        return False
+    return MINIMUM_NAUTOBOT_VERSION.major == 2
 
 
 def load_v2_code_location_changes():
@@ -29,4 +26,26 @@ def load_v2_code_location_changes():
     return changes_map
 
 
+def _read_poetry_lock() -> dict:
+    for directory in (Path.cwd(), *Path.cwd().parents):
+        path = directory / "poetry.lock"
+        if path.exists():
+            return toml.load(Path(path))
+
+    return {}
+
+
+def _read_locked_nautobot_version() -> Optional[str]:
+    poetry_lock = _read_poetry_lock()
+    if not poetry_lock:
+        return None
+
+    for package in poetry_lock.get("package", []):
+        if package["name"] == "nautobot":
+            return package["version"]
+
+    return None
+
+
 MAP_CODE_LOCATION_CHANGES = load_v2_code_location_changes()
+MINIMUM_NAUTOBOT_VERSION = Version(_read_locked_nautobot_version() or metadata.version("nautobot"))
