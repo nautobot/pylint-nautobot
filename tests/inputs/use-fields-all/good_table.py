56cc6bb734275c4cd11db26e34d855fc1ba305b9
Jan Snasel <jan.snasel@networktocode.com>
2024-02-06 17:02:40 +0100

    Use Fields __all__ (#70)
    * feat: nb-use-fields-all
    
    * fix: PR feedback

---
+++ tests/inputs/use-fields-all/good_table.py
@@ -0,0 +1,14 @@
+from nautobot.apps.tables import BaseTable
+from nautobot.apps.tables import ToggleColumn
+from nautobot.extras.tables import StatusTableMixin
+
+
+class MyTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        fields = "__all__"
