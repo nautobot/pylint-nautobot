4f172746308421693d7dc0c5cd151c15bbb099ba
Jan Snasel <jan.snasel@networktocode.com>
2024-02-06 15:09:08 +0000

    fix: PR feedback

--- pylint_nautobot/use_fields_all.py
+++ pylint_nautobot/use_fields_all.py
@@ -10,11 +10,9 @@ from .utils import is_version_compatible
 _META_CLASSES = {
     "nautobot.core.api.serializers.NautobotModelSerializer.Meta": ">1",
     "nautobot.core.tables.BaseTable.Meta": ">=2",
-    "nautobot.extras.filters.NautobotFilterSet.Meta": ">1",  # Capability should be added, but does not exist today
+    "nautobot.extras.filters.NautobotFilterSet.Meta": ">1",
     "nautobot.extras.forms.NautobotModelForm.Meta": ">1",
     "nautobot.utilities.tables.BaseTable.Meta": ">1,<2",
-    # NautobotBulkEditForm - Unclear, in floor app I see it defined, but others I do not, perhaps it is an implied all, in which case I think better to be explicit and convert to respect the all param as best practices)
-    # NautobotFilterForm - Not needed (can investigate other options for field_order)
 }
 
 _CHECK_CLASSES = [key for key, specifier_set in _META_CLASSES.items() if is_version_compatible(specifier_set)]
